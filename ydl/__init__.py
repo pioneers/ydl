1b4fa41ed384c44fd1612cc3860484552ba6ad9a
Samuel Berkun <sberkun@berkeley.edu>
2022-05-18 05:03:42 -0700

    better command-line interface

--- ydl/__init__.py
+++ ydl/__init__.py
@@ -1,9 +1,21 @@
-"""
-Import ydl_send and stuff (TODO: docs)
+"""YDL: interprocess communication over TCP sockets
 
-    >>> from ydl import ydl_send, todo
-    >>> ydl_send("process 2", "banana", {"data1":1, "data2":2})
+Import the ydl module to initialize a YDL client or server: 
 
+Process 0:
+    >>> from ydl import run_ydl_server
+    >>> run_ydl_server()
+
+Process 1:
+    >>> from ydl import YDLClient
+    >>> yc = YDLClient()
+    >>> yc.send(("process 2", "stuff"))
+
+Process 2:
+    >>> from ydl 
+    >>> yc = YDLClient("process 2")
+    >>> yc.receive()
+    ('process 2', 'stuff')
 """
 
 from ._core import DEFAULT_YDL_ADDR, YDLClient, run_ydl_server
--- ydl/__main__.py
+++ ydl/__main__.py
@@ -1,92 +1,27 @@
+"""
+Run a YDL server, which clients can use to communicate.
+"""
+
+import argparse
+import signal
 import sys
 from ._core import run_ydl_server
 
-
-
-
-def print_help_message():
-    s = """
-Usage: ydl [-h] [-v] [-a address] [-p port]
-
-Command-line options:
-     -h --help
-        Display a help message and exit
-     -v --verbose
-        Print whenever a client connects or disconnects
-     -a --address address
-        Run server on specified ip address, such as 0.0.0.0
-     -p --port port
-        Run server on specified port 
-    """
-    print(s)
-
-
-
-
 if __name__ == "__main__":
-
-    flags = {
-        "verbose": False,
-        "help": False,
-    }
-    arg_flags = {
-        "address": None,
-        "port": None,
-    }
-    short_flags = {
-        "v":"verbose",
-        "h": "help",
-        "a":"address",
-        "p":"port",
-    }
-
-    ind = 1
-    usage_error = None
-    while ind < len(sys.argv):
-        onedash = sys.argv[ind].startswith("-")
-        twodash = sys.argv[ind].startswith("--")
-        f = None
-        if onedash and not twodash:
-            if sys.argv[ind][1:] in short_flags:
-                f = short_flags[sys.argv[ind][1:]]
-            else:
-                usage_error = f"Short flag not recognized: {sys.argv[ind]}"
-                break;
-        elif onedash:
-            f = sys.argv[ind][2:]
-        
-        if f is not None:
-            if f in flags:
-                flags[f] = True
-            elif f in arg_flags:
-                ind += 1
-                if ind < len(sys.argv) and not sys.argv[ind].startswith("-"):
-                    arg_flags[f] = sys.argv[ind]
-                else:
-                    usage_error = f"No argument for: {f}"
-                    break
-            else:
-                usage_error = f"Flag not recognized: {f}"
-                break
-        else:
-            # regular arg, but ydl has none
-            usage_error = f"Parameter not recognized: {sys.argv[ind]}"
-            break;
-        ind += 1
-
-    if usage_error is not None:
-        print("Error:", usage_error)
-        print_help_message()
-    elif flags["help"]:
-        print_help_message()
-    else:
-        p = None
-        good = True
-        if arg_flags["port"] is not None:
-            try:
-                p = int(arg_flags["port"])
-            except ValueError:
-                print(f"Error parsing port number: {arg_flags['port']}")
-                good = False
-        if good:
-            run_ydl_server(arg_flags["address"], p, flags["verbose"])
+    parser = argparse.ArgumentParser(prog='python3 -m ydl', description=__doc__)
+    parser.add_argument("-s", "--silent", action='store_true',
+        help="do not print client connections")
+    parser.add_argument("-a", "--address", default=None, type=str,
+        help="run server on specified ip address, such as 0.0.0.0")
+    parser.add_argument("-p", "--port", default=None, type=int,
+        help="run server on specified port")
+    
+    args = parser.parse_args()
+    verbose = not args.silent
+    def sigint_handler(_signum, _frame):
+        if verbose:
+            print("\nShutting down YDL server")
+        sys.exit(0)
+    
+    signal.signal(signal.SIGINT, sigint_handler)
+    run_ydl_server(args.address, args.port, verbose)
